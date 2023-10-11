445e7ea0dc0f015e0d974972caa87dc9a4e9672d
OlexandrJ <134488937+OlexandrJ@users.noreply.github.com>
2023-10-11 12:14:00 +0000

    Deploying to gh-pages from @ OlexandrJ/goit-js-hw-09@f20da6decf3a24ea04e72c7351506d9313f48a39 🚀

---
+++ 01-color-switcher.b1a26785.js
@@ -0,0 +1,3 @@
+!function(){let e;let t=document.querySelector("[data-start]"),d=document.querySelector("[data-stop]"),a=document.body;t.addEventListener("click",()=>{t.disabled=!0,d.disabled=!1,e=setInterval(()=>{let e=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,"0")}`;a.style.backgroundColor=e},1e3)}),d.addEventListener("click",()=>{t.disabled=!1,d.disabled=!0,clearInterval(e)})}();//# sourceMappingURL=01-color-switcher.b1a26785.js.map
+
+//# sourceMappingURL=01-color-switcher.b1a26785.js.map
