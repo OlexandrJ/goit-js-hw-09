e368d260aa9d3d2fba1ea3ac5531e948f28b81c9
OlexandrJ <134488937+OlexandrJ@users.noreply.github.com>
2023-10-13 09:03:07 +0000

    Deploying to gh-pages from @ OlexandrJ/goit-js-hw-09@b4ea8a95df2b2f1600b71efe2b57eb930b61c791 🚀

--- 03-promises.5fdfb18c.js
+++
@@ -1,3 +0,0 @@
-new Promise((o,e)=>{let i=Math.random()>.3;setTimeout(()=>{i?o({position:2,delay:1500}):e({position:2,delay:1500})},1500)}).then(({position:o,delay:e})=>{console.log(`✅ Fulfilled promise ${o} in ${e}ms`)}).catch(({position:o,delay:e})=>{console.log(`❌ Rejected promise ${o} in ${e}ms`)});//# sourceMappingURL=03-promises.5fdfb18c.js.map
-
-//# sourceMappingURL=03-promises.5fdfb18c.js.map
